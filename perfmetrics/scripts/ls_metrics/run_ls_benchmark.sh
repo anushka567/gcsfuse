d72f88c79468310642825b4bcafc4517d2e2d39e
anushka567 <anushkadhn@google.com>
2023-06-06 06:26:03 +0000

    changes for indv tests

--- perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
+++ perfmetrics/scripts/ls_metrics/run_ls_benchmark.sh
@@ -8,7 +8,7 @@ sudo apt-get install pip -y
 echo Installing requirements..
 pip install --require-hashes -r requirements.txt --user
 echo Running script..
-python3 listing_benchmark.py config.json --command "ls -R" --num_samples 30 --message "Testing latencies with gcsObject"
+python3 listing_benchmark.py config.json --command "ls -R" --num_samples 2 --message "Testing latencies with gcsObject"
 # Upload data to the gsheet only when it runs through kokoro.
 #if [ "${KOKORO_JOB_TYPE}" != "RELEASE" ] && [ "${KOKORO_JOB_TYPE}" != "CONTINUOUS_INTEGRATION" ] && [ "${KOKORO_JOB_TYPE}" != "PRESUBMIT_GITHUB" ];
 #then
